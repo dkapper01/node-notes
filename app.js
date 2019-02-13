7691f02aa62f8aa2e56a815a0937e8861487f65f
Daniel Kapper <dkapper01@gmail.com>
2019-02-12 23:05:17 -0500

    build out read all remove add in notes.js

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const yargs = require('yargs');
 const notes = require("./notes");
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0]
 console.log("Command: " + command);
 console.log("Process", process.argv); 
 console.log("Yargs", argv); 
 
 if(command ===  'add') {
-  console.log("Adding new note"); 
+  notes.addNote(argv.title, argv.body);
 } else if (command === "list") {
-  console.log("listing all notes"); 
+  notes.getAll();  
 } else if (command === "read") {
-  console.log("Reading note"); 
+  notes.getNote(argv.title);  
 } else if (command === "remove") {
-  console.log("Removing note")
+  notes.removeNote(argv.title); 
 }
 else {
   console.log("command not known"); 
--- notes.js
+++ notes.js
@@ -1,8 +1,24 @@
 console.log("Strating notes.js");
 
-module.exports.addNote = () => {
-  console.log("add note");
-  return "New note";
-};
+var addNote = (title, body)  => {
+  console.log(`Daniel ${title} is the boss ${body}`); 
+}
 
+var getAll = () => {
+  console.log("getting all notes"); 
+}
+
+var getNote = (title) => {
+  console.log(`Getting note ${title}`)
+}
+
+var removeNote = (title) => {
+  console.log(`Removing note ${title}`);
+}
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+}
 
